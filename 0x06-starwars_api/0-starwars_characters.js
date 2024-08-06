f71b3f1897fb3db2aa90b03463d1cc3b58438502
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-08-05 21:43:25 -0700

    task: modify the response gotten from a REST API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
+
+request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
